a7ec29607d3a551c46419c1b4226059cde9cfcd3
DudekW <wojciechsbox@gmail.com>
2017-05-10 11:42:51 +0200

    clear code

--- velmwheel_core/src/VelmWheel_core.cpp
+++ velmwheel_core/src/VelmWheel_core.cpp
@@ -10,9 +10,11 @@
 
 #include "VelmWheel_core.h"
 
-const double a = 0.3775;
-const double b = 0.36;
-const double r = 0.1016;
+// Mobile platform constants
+const double a = 0.3775; // 1/2 length along Y axis
+const double b = 0.36; // 1/2 length along X axis
+const double r = 0.1016; // wheel radius
+
 // gear 1/50 | 10000 encoder counts
 // (50 * 10000/ 2 * pi) 
 const int rad_to_encoder_scalar = 50 * 5000 / 3.1415;
@@ -195,19 +197,11 @@ void setHeader(std_msgs::Header &header, const std::string &frame, const uint32_
 	header.seq = seq_id;
 }
 
-// wheels: 0 - rl, 1 - fl, 2 - fr, 3 - rr | [rad/s]
-// void getWheelsSpeed()
-// {
-// 	wheels_speed_enc[0] = (double)(wrl_dist_diff / (double)(nsec.count() - nsec_old.count()) ) *1000000000;
-// 	wheels_speed_enc[1] = (double)(wfl_dist_diff / (double)(nsec.count() - nsec_old.count())) *1000000000;
-// 	wheels_speed_enc[2] = (double)(wfr_dist_diff / (double)(nsec.count() - nsec_old.count())) *1000000000;
-// 	wheels_speed_enc[3] = (double)(wrr_dist_diff / (double)(nsec.count() - nsec_old.count())) *1000000000;
-// 	RTT::Logger::log() << RTT::Logger::Warning << "enc_0: "<<wheels_speed_enc[0]<<RTT::Logger::nl;
-// }
 
 // wheels: 0 - rl, 1 - fl, 2 - fr, 3 - rr
 void getOdom()
 {
+
 /*
 	(wheels_speed_enc[0] + wheels_speed_enc[1]) * r = 2 * X + 2 * T
 	(wheels_speed_enc[2] + wheels_speed_enc[3]) * r = -2 * X + 2* T 
@@ -221,21 +215,11 @@ void getOdom()
 	// wheels: 0 - rl, 1 - fl, 2 - fr, 3 - rr
 	//
 
-
-	///////
-	////   z równań wychodzi 4(a+b), z doświadczeń 8(a+b) !!!!!!!!!!!!!!!!!!!!!
-	//////
-	/////
 	msg_odometry.twist.twist.angular.z = (wheels_speed_enc[0] + wheels_speed_enc[1] + wheels_speed_enc[2] + wheels_speed_enc[3]) * ( r / (4*(a + b)));
 
 	msg_odometry.twist.twist.linear.x = ((wheels_speed_enc[0] + wheels_speed_enc[1]) * r  - 2 * msg_odometry.twist.twist.angular.z * (a + b)) / 2;
 	msg_odometry.twist.twist.linear.y = wheels_speed_enc[1] * r - msg_odometry.twist.twist.linear.x - msg_odometry.twist.twist.angular.z * (a + b);
-	////
-	//  vel_loop time inny dzielnik czasu !!!!!!
-	///
-	euler_z =  euler_z + (wfl_dist_diff + wfr_dist_diff + wrl_dist_diff + wrr_dist_diff) * (r  / (4 * (a + b)));//msg_odometry.twist.twist.angular.z * (double) ( (double) vel_loop_time/(double) 100000000);//msg_odometry.twist.twist.angular.z * (wrl_dist_diff + wfl_dist_diff + wfr_dist_diff + wrr_dist_diff) * ( r*r / (4*(a + b)));
-	//RTT::Logger::log() << RTT::Logger::Warning << "euler: "<<euler_z<<RTT::Logger::nl;
-	// RTT::Logger::log() << RTT::Logger::Warning << "time: "<<(double) (vel_loop_time/1000000000)<<RTT::Logger::nl;
+	euler_z =  euler_z + (wfl_dist_diff + wfr_dist_diff + wrl_dist_diff + wrr_dist_diff) * (r  / (4 * (a + b)));
 	odom_quat.setRPY(0, 0, euler_z);
 
 	msg_odometry.pose.pose.orientation.x = odom_quat.x();
@@ -243,11 +227,9 @@ void getOdom()
 	msg_odometry.pose.pose.orientation.z = odom_quat.z();
 	msg_odometry.pose.pose.orientation.w = odom_quat.w();
 
-	msg_odometry.pose.pose.position.x =  msg_odometry.pose.pose.position.x + ((wrl_dist_diff + wfl_dist_diff) * r - 2 * (euler_z - euler_old) * (a + b)) / 2;// msg_odometry.pose.pose.position.x + msg_odometry.twist.twist.linear.x * (double) (vel_loop_time/1000000000);
-
-	msg_odometry.pose.pose.position.y =  msg_odometry.pose.pose.position.y + wfl_dist_diff * r - (msg_odometry.pose.pose.position.x - position_x_old) - (euler_z - euler_old) * (a + b);//msg_odometry.pose.pose.position.y + msg_odometry.twist.twist.linear.y * vel_loop_time/1000000000;//wfl_dist_diff * r - msg_odometry.pose.pose.position.x - euler_z;
+	msg_odometry.pose.pose.position.x =  msg_odometry.pose.pose.position.x + ((wrl_dist_diff + wfl_dist_diff) * r - 2 * (euler_z - euler_old) * (a + b)) / 2;
+	msg_odometry.pose.pose.position.y =  msg_odometry.pose.pose.position.y + wfl_dist_diff * r - (msg_odometry.pose.pose.position.x - position_x_old) - (euler_z - euler_old) * (a + b);
 	msg_odometry.pose.pose.position.z = 0;
-	//RTT::Logger::log() << RTT::Logger::Warning << "diff X: "<<(msg_odometry.pose.pose.position.x - position_x_old)<<RTT::Logger::nl;
 
 	euler_old = euler_z;
 	position_x_old = msg_odometry.pose.pose.position.x;
@@ -289,7 +271,6 @@ void VelmWheelCore::updateHook()
 
 		wfl_dist_diff = (msg_wfl_enc_pos_ - wfl_enc_old) / rad_to_encoder_scalar; 
 		wfl_enc_old = msg_wfl_enc_pos_ ;
-		//RTT::Logger::log() << RTT::Logger::Warning << "1: "<<wfl_dist_diff <<RTT::Logger::nl;
 
 	}
 	if (RTT::NewData == in_wrl_enc_pos_.read(msg_wrl_enc_pos_))
@@ -297,7 +278,6 @@ void VelmWheelCore::updateHook()
 
 		wrl_dist_diff = (msg_wrl_enc_pos_ - wrl_enc_old) / rad_to_encoder_scalar; 
 		wrl_enc_old = msg_wrl_enc_pos_ ;
-		//RTT::Logger::log() << RTT::Logger::Warning << "2: "<<wrl_dist_diff <<RTT::Logger::nl;
 
 	}
 	if (RTT::NewData == in_wrr_enc_pos_.read(msg_wrr_enc_pos_))
@@ -305,65 +285,43 @@ void VelmWheelCore::updateHook()
 
 		wrr_dist_diff = (msg_wrr_enc_pos_ - wrr_enc_old) / rad_to_encoder_scalar; 
 		wrr_enc_old = msg_wrr_enc_pos_ ;
-		//RTT::Logger::log() << RTT::Logger::Warning << "3: "<<wrr_dist_diff <<RTT::Logger::nl;
-
 	}
 	if (RTT::NewData == in_wfr_enc_pos_.read(msg_wfr_enc_pos_))
 	{
 
 		wfr_dist_diff = (msg_wfr_enc_pos_ - wfr_enc_old) / rad_to_encoder_scalar; 
 		wfr_enc_old = msg_wfr_enc_pos_ ;
-		//RTT::Logger::log() << RTT::Logger::Warning << "4: "<<wfr_dist_diff <<RTT::Logger::nl;
 
 	}	
-	// Read encoder data and calculate distance diff [m]  ||   Do all sensors return data at the same time???????   ||
+	// Read encoder data and calculate distance diff [m]
 	if (RTT::NewData == in_wrl_enc_vel_.read(msg_wrl_enc_vel_))
 	{
 		sequence_enc += 1;
-		// RTT::Logger::log() << RTT::Logger::Warning << "wrl_vel_: "<<wrl_speed/ (2* rad_to_encoder_scalar)<<RTT::Logger::nl;
-
-		// RTT::Logger::log() << RTT::Logger::Warning << "msg_wrl_enc_vel_: "<<msg_wrl_enc_vel_ / (2* rad_to_encoder_scalar) <<RTT::Logger::nl;
-
-		// nsec = std::chrono::duration_cast<std::chrono::nanoseconds >(std::chrono::system_clock::now().time_since_epoch());
-		// vel_loop_time = nsec.count() - nsec_old.count();
 
 		wheels_speed_enc[0] = msg_wrl_enc_vel_/ (rad_to_encoder_scalar);
-		// wrl_dist_diff = (double) ((msg_wrl_enc_vel_ - wrl_enc_vel_old) / (double)((50*5000))) * 3.1415; 
-		// RTT::Logger::log() << RTT::Logger::Warning << "wrl_dist_diff: "<<wrl_dist_diff<<RTT::Logger::nl;
-		// wrl_enc_vel_old = msg_wrl_enc_vel_;
 	}
 	if (RTT::NewData == in_wrr_enc_vel_.read(msg_wrr_enc_vel_))
 	{
 		wheels_speed_enc[3] = msg_wrr_enc_vel_/ (rad_to_encoder_scalar);
 
-		// wrr_dist_diff = ( (msg_wrr_enc_vel_ - wrr_enc_vel_old) / (50*5000) ) * ( (2 * 3.1415 * r) / 360 );
-		// wrr_enc_vel_old = msg_wrr_enc_vel_;
 	}
 	if (RTT::NewData == in_wfl_enc_vel_.read(msg_wfl_enc_vel_))
 	{
 		wheels_speed_enc[1] = msg_wfl_enc_vel_/ (rad_to_encoder_scalar);
 
-		// // wfl_dist_diff = ( (msg_wfl_enc_vel_ - wfl_enc_vel_old) / (50*5000) ) * ( (2 * 3.1415 * r) / 360 );
-		// wfl_enc_vel_old = msg_wfl_enc_vel_;
 	}
 	if (RTT::NewData == in_wfr_enc_vel_.read(msg_wfr_enc_vel_))
 	{
 		wheels_speed_enc[2] = msg_wfr_enc_vel_/ (rad_to_encoder_scalar);
 
-		// wfr_dist_diff = ( (msg_wfr_enc_vel_ - wfr_enc_vel_old) / (50*5000) ) * ( (2 * 3.1415 * r) / 360 );
-		// wfr_enc_vel_old = msg_wfr_enc_vel_;
 }
 	// set odometry msg header
 	nsec = std::chrono::duration_cast<std::chrono::nanoseconds >(std::chrono::system_clock::now().time_since_epoch());
 	setHeader(msg_odometry.header, "odom", sequence_enc);
-	// get speed of wheels [m/s]
-	// getWheelsSpeed();
-	// get base pose and twist from odometry
+	// get base pose and twist from encoders data
 	getOdom();
 	// write odometry msg to output port
 	out_odometry_.write(msg_odometry);
-
-	// nsec_old = nsec;
 
 }
 
